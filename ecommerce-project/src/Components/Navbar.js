2a8180c1dfbc7d0a48a6b3e9ecc1b22a31b4230b
nix <nickinazo.cam@gmail.com>
2023-06-14 21:46:28 +0100

    REACTAAAAAH

--- ecommerce-project/src/Components/Navbar.js
+++ ecommerce-project/src/Components/Navbar.js
@@ -1,5 +1,14 @@
 import React from "react";
+import * as FaIcons from "react-icons/fa";
 
 function Navbar() {
-    
+    return (
+        <>
+        <div classNAme="navbar">
+            <Link to="#" className='menu-bars'>
+
+            </Link>
+        </div>
+        </>
+    )
 }
