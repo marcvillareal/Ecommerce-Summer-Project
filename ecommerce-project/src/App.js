c03f8a4d878a9261aaac746a673b24fe36cd4491
nix <nickinazo.cam@gmail.com>
2023-06-25 23:07:53 +0100

    page routing

--- ecommerce-project/src/App.js
+++ ecommerce-project/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import { Home } from './Pages/Home';
+import { Contact } from './Pages/Contact';
+import { Portfolio } from './Pages/Portfolio';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path ="/" element={<Home/>}/>
+        <Route path ="/contact" element={<Contact/>}/>
+        <Route path ="/portfolio" element={<Portfolio/>}/>
+      </Routes>
+      
       <h1>Dee InkFlow</h1>
       </div>
   );
---
+++ ecommerce-project/src/Pages/Contact.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Contact = () => {
+    return <div>ContactUs</div>;
+};
