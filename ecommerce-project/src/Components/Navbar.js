6594a9594d427a89c4491f04eb19cdb50fb6c4af
nix <nickinazo.cam@gmail.com>
2023-06-25 22:52:24 +0100

    page routing

--- ecommerce-project/src/Components/Navbar.js
+++ ecommerce-project/src/Components/Navbar.js
@@ -1,14 +1,7 @@
 import React from "react";
 import * as FaIcons from "react-icons/fa";
 
-function Navbar() {
-    return (
-        <>
-        <div classNAme="navbar">
-            <Link to="#" className='menu-bars'>
-
-            </Link>
-        </div>
-        </>
-    )
-}
+export const Navbar = () => {
+    
+    return 
+};
