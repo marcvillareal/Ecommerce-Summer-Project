d42c33c6b1f6186e4225ac81d8964176b27b7d4f
marcvillareal <105477620+marcvillareal@users.noreply.github.com>
2023-07-08 04:37:08 +0100

    Implemented carousel/slideshow to pages

---
+++ ecommerce-project/src/Components/Images.jsx
@@ -0,0 +1,24 @@
+//import images here for carousel
+import Image1 from './../Images/image1.jpeg'
+import Image2 from './../Images/image2.jpeg'
+import Image3 from './../Images/image3.jpeg'
+
+const Images = [
+    {
+      id: 1,
+      src: Image1,
+      alt: "Image 1",
+    },
+    {
+      id: 2,
+      src: Image2,
+      alt: "Image 2",
+    },
+    {
+      id: 3,
+      src: Image3,
+      alt: "Image 3",
+    },
+  ];
+  
+  export default Images;
